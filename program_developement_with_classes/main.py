0a4a41b97fe6b79790cd09591074832a4c537940
Joe Paris <joe.paris@outlook.com>
2020-02-04 22:31:22 -0800

    Demo

--- program_developement_with_classes/main.py
+++ program_developement_with_classes/main.py
@@ -6,6 +6,5 @@ import island
 if __name__ == "__main__":
     royale = island.Island(10)
     animal1 = animal.Animal(island=royale, x=0, y=8, name="a1")
-    print(animal1.position())
-
+    print(animal1.position)
     print(royale)
