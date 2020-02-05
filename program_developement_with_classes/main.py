f0114e92396c6eeb7427974d0b76ea0c1a0703ac
Joe Paris <joe.paris@outlook.com>
2020-02-04 22:19:31 -0800

    Demo changes

--- program_developement_with_classes/main.py
+++ program_developement_with_classes/main.py
@@ -1,8 +1,11 @@
 #! /usr/bin/env python3.7
 
-import island_v1
+import animal
+import island
 
 if __name__ == "__main__":
-    royale = island_v1.Island(10)
+    royale = island.Island(10)
+    animal1 = animal.Animal(island=royale, x=0, y=8, name="a1")
+    print(animal1.position())
 
     print(royale)
